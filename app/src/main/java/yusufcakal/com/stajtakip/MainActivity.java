dce1e05e2130f960d10f04c7709a8a62eb209fc0
yusufcakal <info@yusufcakal.com>
2018-05-08 21:49:32 +0300

    login commit

--- app/src/main/java/yusufcakal/com/stajtakip/MainActivity.java
+++ app/src/main/java/yusufcakal/com/stajtakip/MainActivity.java
@@ -2,15 +2,39 @@ package yusufcakal.com.stajtakip;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity 
+    implements View.OnClickListener{
+
+    private EditText etEmail, etPassword;
+    private Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTitle(getResources().getText(R.string.login));
 
-        //test
+        etEmail = findViewById(R.id.etEmail);
+        etPassword = findViewById(R.id.etPassword);
+        btnLogin = findViewById(R.id.btnLogin);
+        
+        btnLogin.setOnClickListener(this);
+
 
     }
+
+    @Override
+    public void onClick(View view) {
+        String email = etEmail.getText().toString();
+        String password = etPassword.getText().toString();
+
+
+
+    }
+    
 }
