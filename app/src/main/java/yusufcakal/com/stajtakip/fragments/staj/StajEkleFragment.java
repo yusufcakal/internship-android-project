3c0e135c96e5b4e6f22ce0efc8b8d67fc97c1327
yusufcakal <info@yusufcakal.com>
2018-05-27 02:40:07 +0300

    staj ekle spinner

--- app/src/main/java/yusufcakal/com/stajtakip/fragments/staj/StajEkleFragment.java
+++ app/src/main/java/yusufcakal/com/stajtakip/fragments/staj/StajEkleFragment.java
@@ -5,6 +5,7 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
 import yusufcakal.com.stajtakip.R;
@@ -24,8 +25,11 @@ public class StajEkleFragment extends android.support.v4.app.Fragment{
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_stajekle, container, false);
 
+        spinner = view.findViewById(R.id.spinner);
 
-
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_spinner_item, firmalar);
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(dataAdapter);
 
         return view;
     }
