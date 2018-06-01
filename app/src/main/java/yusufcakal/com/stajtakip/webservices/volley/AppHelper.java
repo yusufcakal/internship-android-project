7cf44ac0f2cba4dc302d002487345e02c5c48666
yusufcakal <info@yusufcakal.com>
2018-06-02 01:40:49 +0300

    volley multipart added

---
+++ app/src/main/java/yusufcakal/com/stajtakip/webservices/volley/AppHelper.java
@@ -0,0 +1,30 @@
+package yusufcakal.com.stajtakip.webservices.volley;
+
+/**
+ * Created by Yusuf on 2.06.2018.
+ */
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.support.v4.content.ContextCompat;
+import java.io.ByteArrayOutputStream;
+
+public class AppHelper {
+
+    public static byte[] getFileDataFromDrawable(Context context, int id) {
+        Drawable drawable = ContextCompat.getDrawable(context, id);
+        Bitmap bitmap = ((BitmapDrawable) drawable).getBitmap();
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG, 0, byteArrayOutputStream);
+        return byteArrayOutputStream.toByteArray();
+    }
+
+    public static byte[] getFileDataFromDrawable(Context context, Bitmap bitmap) {
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 80, byteArrayOutputStream);
+        return byteArrayOutputStream.toByteArray();
+    }
+
+}
